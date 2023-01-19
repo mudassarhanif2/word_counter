e9ae66006539890b596d26f07b0537dac1420b03
Mudassar Hanif <mudassarhanif2@gmail.com>
2023-01-19 17:49:04 +0500

    working fine

---
+++ index.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from 'chalk-animation';
+
+const sleep = () => {
+    return new Promise ((res, rej)=>{
+        setTimeout(res, 1000)
+    })
+}
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow("-------Lets start the program------")
+    await sleep();
+    rainbowTitle.stop()
+    askQuestion()
+}
+
+welcome();
+
+interface AskQuestion {
+    str: string,
+}
+
+async function askQuestion() {
+    let question = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'str',
+            message: chalk.greenBright('Please enter the paragraph you want to count word and characters: ')
+        }
+    ])
+    let wordArr = question.str.split(' ');
+    let arr = []
+    let chr = 0
+    for(let i of wordArr){
+        if(i){
+            arr.push(i)
+            chr += i.length
+        }
+        else{
+            continue
+        }
+    }
+    console.log(`Total words in the paragraph are: ${arr.length}`)
+    console.log(`Total chracters in the paragraph are: ${chr}`)
+}
+
+
---
+++ test.js
@@ -0,0 +1,11 @@
+let arr = 'as dfa   b';
+// let brr = arr.split()
+for (let i of arr.split(' ')) {
+    if (i) {
+        console.log(i);
+    }
+    else {
+        continue;
+    }
+}
+export {};
